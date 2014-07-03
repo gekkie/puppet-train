bc16d44c6bd4556c172c87fd03ddeed30285a639
geordy <gkorte@gmail.com>
2014-07-03 17:19:27 +0200

    Added ERB

--- apache.pp
+++ apache.pp
@@ -7,14 +7,12 @@ $MyName = 'Geordy'
 file { '/var/www/index.html':
   ensure => file,
   mode => 644,
-  content => "<html><body><h1>This is ${MyName}'s Webserver</h1>
-<p>My FQDN is ${fqdn}, and my IP address is ${ipaddress}</p>
-<p>And I run on this great Operating System called ${operatingsystem}</p>
-</body></html>",
+  content => template('/etc/puppet/manifests/html_index.erb'),
 }
 service { 'apache2':
   ensure => running,
   enable => true,
   subscribe => File['/var/www/index.html'],
 } 
+
 
